bf0ba0d19bea08f92b77c8fe538f2e12175f6d53
unknown <s.luisa@aluno.ifsp.edu.br>
2023-08-18 16:37:23 -0300

    Adicionando o nome AnaLuisa

--- src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
+++ src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
@@ -1,8 +1,12 @@
 package edu.ifsp.supportingwomen.swapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PontosDeApoioController {
-    
+    @GetMapping("/Ana Luisa")
+     public String  endPoint1(){
+        return "Ana dos brigadeiros";
+     }
 }
