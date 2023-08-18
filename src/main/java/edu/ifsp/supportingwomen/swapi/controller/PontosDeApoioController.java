d7a3ddf05280c7a9200a247845980c614a7018bb
unknown <isabelly.nathalia@aluno.ifsp.edu.br>
2023-08-18 16:38:12 -0300

    Rota da Isabelly

--- src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
+++ src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
@@ -1,8 +1,16 @@
 package edu.ifsp.supportingwomen.swapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PontosDeApoioController {
-    
+    @GetMapping("/isabelly")
+    String meuMetodo(){
+        return "Feito pela Isabelly";
+    }
+    @GetMapping("isabelly2")
+    String metodo2(){
+        return "Esse sim feito pela Isabelly";
+    }
 }
