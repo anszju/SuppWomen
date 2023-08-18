f406f8f9a9b57baa23afd32db62ac699cd4ce6b9
unknown <indkkjhg@gmail.com>
2023-08-18 16:41:14 -0300

    oi

--- src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
+++ src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
@@ -1,8 +1,13 @@
 package edu.ifsp.supportingwomen.swapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
 
 @RestController
 public class PontosDeApoioController {
-    
+    @GetMapping("/Ingrid")
+    public String endPoint1(){
+        return "exemplo ind";
+    }
 }
