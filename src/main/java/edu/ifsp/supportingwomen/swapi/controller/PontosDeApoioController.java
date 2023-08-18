642d5f73b0a15ed1c40aef3958a245a470fbb83a
unknown <e.andrade@aluno.ifsp.edu.br>
2023-08-18 16:30:33 -0300

    Rota Ana adicionado

--- src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
+++ src/main/java/edu/ifsp/supportingwomen/swapi/controller/PontosDeApoioController.java
@@ -1,8 +1,12 @@
 package edu.ifsp.supportingwomen.swapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PontosDeApoioController {
-    
+    @GetMapping("/AnaJulia")
+    String metodoAnaju(){
+        return "Ana Julia fez esse método ;)";
+    }
 }
