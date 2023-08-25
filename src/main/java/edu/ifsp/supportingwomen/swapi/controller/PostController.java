8ec5a533e3bd4d5f910977c21e10b46212006f61
Yukio Shiraishi <rafaelyukioshira@gmail.com>
2023-08-25 20:30:11 -0300

    post post

--- src/main/java/edu/ifsp/supportingwomen/swapi/controller/PostController.java
+++ src/main/java/edu/ifsp/supportingwomen/swapi/controller/PostController.java
@@ -3,6 +3,8 @@ package edu.ifsp.supportingwomen.swapi.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import edu.ifsp.supportingwomen.swapi.model.Post;
 import edu.ifsp.supportingwomen.swapi.model.PostDAO;
@@ -13,5 +15,12 @@ public class PostController {
     @GetMapping("/suppwomen/posts")
     List<Post> mostrarPosts(){
         return PostDAO.getInstance().read();
+    }
+
+        @PostMapping("/suppwomen/posts")
+        public Post cadPosts(@RequestBody Post novoPost){ 
+            PostDAO postDAO = PostDAO.getInstance();
+            postDAO.create(novoPost);
+            return novoPost;
     }
 }
